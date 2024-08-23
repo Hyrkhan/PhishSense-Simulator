9ccb0f2d3fabd81582c3b14814228946d2b82d74
Hyrkan <hyrkangamez@gmail.com>
2024-08-23 21:24:16 +0800

    made a new script and also the switching of email images and maximizing it

--- Assets/Scripts/GameModeScript.cs
+++ Assets/Scripts/GameModeScript.cs
@@ -2,30 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameModeScript : MonoBehaviour
 {
     public GameObject gameWelcomeScreen;
     public GameObject gameScreen;
-    public GameObject emailScreen;
+    public GameObject emailScreen; // Panel for fullscreen image
+    public Image normalEmailImage; // Image container for normal view
+    public Image fullscreenEmailImage; // Image container for fullscreen view
+
 
     public void ProceedToGame()
     {
         gameWelcomeScreen.SetActive(false);
         gameScreen.SetActive(true);
     }
+
     public void BacktoLevelsScene()
     {
         SceneManager.LoadScene("LevelsScene");
     }
+
     public void MaximizeScreen()
     {
+        // Set the image for the fullscreen view
+        fullscreenEmailImage.sprite = normalEmailImage.sprite;
         gameScreen.SetActive(false);
         emailScreen.SetActive(true);
+        
     }
+
     public void MinimizeScreen()
     {
         emailScreen.SetActive(false);
         gameScreen.SetActive(true);
     }
+
+
 }
--- Assets/Scripts/emailNavScript.cs
+++ Assets/Scripts/emailNavScript.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class emailNavScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Email
     {
-        
+        public Sprite emailImage;  // The image representing the email
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Email> emails;  // List of all emails
+    public Image emailDisplay;  // UI element to display the email image
+
+    private int currentIndex = 0;
+
+    private void Start()
     {
-        
+        DisplayEmail(currentIndex);  // Display the first email at the start
     }
+
+    public void NextEmail()
+    {
+        if (currentIndex < emails.Count - 1)
+        {
+            currentIndex++;
+            DisplayEmail(currentIndex);
+        }
+    }
+
+    public void PreviousEmail()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            DisplayEmail(currentIndex);
+        }
+    }
+
+    private void DisplayEmail(int index)
+    {
+        emailDisplay.sprite = emails[index].emailImage;
+    }
+
+
 }
