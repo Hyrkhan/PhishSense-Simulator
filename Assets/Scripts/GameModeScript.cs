a147bd3de815cc4155303635a5d6f8c02bc3b153
Hyrkan <hyrkangamez@gmail.com>
2024-09-02 22:03:16 +0800

    started doing the detection tool functions

--- Assets/Scripts/GameModeScript.cs
+++ Assets/Scripts/GameModeScript.cs
@@ -11,6 +11,7 @@ public class GameModeScript : MonoBehaviour
     public GameObject emailScreen; // Panel for fullscreen image
     public Image normalEmailImage; // Image container for normal view
     public Image fullscreenEmailImage; // Image container for fullscreen view
+    public GameObject detectionScreen;
 
 
     public void ProceedToGame()
@@ -36,6 +37,18 @@ public class GameModeScript : MonoBehaviour
     public void MinimizeScreen()
     {
         emailScreen.SetActive(false);
+        gameScreen.SetActive(true);
+    }
+
+    public void goToDetection()
+    {
+        gameScreen.SetActive(false);
+        detectionScreen.SetActive(true);
+    }
+
+    public void backToGame()
+    {
+        detectionScreen.SetActive(false);
         gameScreen.SetActive(true);
     }
 
