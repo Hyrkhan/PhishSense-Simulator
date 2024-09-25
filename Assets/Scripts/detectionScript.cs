6ba145162bb7825c7c4443a879427c982b44700d
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2024-09-26 03:30:45 +0800

    able to copy and paste the link

--- Assets/Scripts/detectionScript.cs
+++ Assets/Scripts/detectionScript.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class detectionScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text urlLink;
+    public TMP_Text clipboard;
+    public TMP_InputField searchBox;
+    public TMP_InputField VMsearchBox;
+    private string copyLink = "";
 
-    // Update is called once per frame
     void Update()
     {
-        
+        clipboard.text = copyLink;
+    }
+
+    public void CopyLink()
+    {
+        copyLink = urlLink.text;
+        Debug.Log($"Link has been copied: {copyLink}");
+    }
+    public void PasteLink1()
+    {
+        searchBox.text = copyLink;
+    }
+    public void PasteLink2()
+    {
+        VMsearchBox.text = copyLink;
     }
 }
